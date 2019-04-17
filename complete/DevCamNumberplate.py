1e37607c0a697bda55029505d17f4b80d1f8100a
KevinKretschmar <kretsch93@web.de>
2019-04-17 10:56:10 +0200

    code refactored, gitignore added

--- complete/DevCamNumberplate.py
+++ complete/DevCamNumberplate.py
@@ -3,9 +3,12 @@ import json
 import time
 import re, sys, os
 
-mode = "URL"
+mode = "URL" # default mode
+# Azure access point
 APIURL = 'https://westeurope.api.cognitive.microsoft.com/vision/v2.0/recognizeText?mode=Printed'
-key = 'd9bb988b597f47b18602ca2f2701aa68'
+
+# key to Azure Cloud
+key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 imageBaseURL = 'https://raw.githubusercontent.com/volkerhielscher/netnei/master/complete/images/'
 localImagesPath = './images/'
 
@@ -17,17 +20,19 @@ headersLocal = {
     'Ocp-Apim-Subscription-Key': key }
 
 jsonData = {"url": imageBaseURL + "bild1.jpg"}
-    
+
+# contains every file in the specified path
 directory = os.listdir(localImagesPath)
 
-
 def getText (url):
-    time.sleep(3)
+    time.sleep(3) # give Azure time to compute
     try:
         i = 0
+        # get the response of the image recognition
         request2 = requests.get(url, headers=headersLocal)
         print ('STATUSTEXT: ')
         print (request2.text)
+        # test, if Azure needs more computing time
         while(request2.json()['status'] == 'Running' or request2.json()['status'] == 'Not started'):
             print ('STATUSTEXT: ' + request2.text)
             print ("Loop "+str(i))
@@ -37,8 +42,11 @@ def getText (url):
             except requests.exceptions.RequestException as e:
                 print (e)
             time.sleep(2)
+        # lines is a array that holds all the recognized text
         for line in request2.json()['recognitionResult']['lines']:
+            # remove small o's as they are misrecognized circles
             text = re.sub('o', '', line['text'])
+            # search for german number plate via regular expression
             match = re.search("[A-ZÖÜÄ]{1,3}[ |-][A-ZÖÜÄ]{1,2}[ |-][0-9]{1,4}[E|H]?", text)
             if (match):
                 print("Plate: "+ text)
@@ -47,71 +55,60 @@ def getText (url):
 
     except requests.exceptions.RequestException as e:
         print (e)
+    except Exception as e:
+        print ('Error in getText():')
+        print (e)
+
+def postToCloud (mode, file):
+    # check how to access files
+    try:
+
+        if mode == 'local':
+            # open image as binary and post it to the Azure Cloud 
+            data = open( localImagesPath + file, 'rb').read()
+            print("File opened")
+            request = requests.post(APIURL, headers=headersLocal, data=data, timeout=10)    
+        elif mode == 'URL':
+            # use images from the github remote repository
+            jsonData = {"url": imageBaseURL + file}
+            request = requests.post(APIURL, headers=headersURL, json=jsonData, timeout=10)        
+        else:
+            print ('Error: PostToCloud() was called with wrong mode')
+            return
+
+    except Exception as e:
+        print ('Error in postToCloud():')
+        print (e)
+        return    
+
+    # call getText() to get the result from Azure. The desired URL is sent as part of the headers.
+    try:
+        reqHeader = request.headers
+        url = reqHeader['Operation-Location']
+        getText(url)
+    except Exception as e:
+        print ('Error in postToCloud():')
+        print (e)
 
 def uploadIMG():
-    try:
-        print ("Mode: " + mode)
-        if(mode == 'local'):
-            if(len(sys.argv) <= 2):
-                for file in directory:
-                    print(file + ' :')
-                    try:    
-                        data = open( localImagesPath + file, 'rb').read()
-                        print("File opened")
-                        request = requests.post(APIURL, headers=headersLocal, data=data, timeout=10)
-                        reqHeader = request.headers
-                        url = reqHeader['Operation-Location']
-                        getText(url)
-                    except Exception as e:
-                        print(e)
-                        
-            else:
-                try:
-                    data = open (localImagesPath + sys.argv[2], 'rb').read()
-                    print ('File opened')
-                except Exception as e:
-                    print(e)
-                request = requests.post(APIURL, headers=headersLocal, data=data, timeout=10)
-                reqHeader = request.headers
-                url = reqHeader['Operation-Location']
-                getText(url)
-        else:
-            if (len(sys.argv) < 2 ):
-                for file in directory:
-                    print (file + ' :------------------------------------------------------------------')
-                    try:
-                        jsonData = {"url": imageBaseURL + file}
-                        request = requests.post(APIURL, headers=headersURL, json=jsonData, timeout=10)
-                        reqHeader = request.headers
-                        url = reqHeader['Operation-Location']
-                        getText(url)
-                    except Exception as e:
-                        print ('EXCEPTION:')
-                        print(e)
-            elif (len(sys.argv) == 2 and sys.argv[1] != 'local' and sys.argv[1] != 'URL'):
-                jsonData = {"url": imageBaseURL + sys.argv[1]}
-                request = requests.post(APIURL, headers=headersURL, json=jsonData, timeout=10)
-                reqHeader = request.headers
-                url = reqHeader['Operation-Location']
-                getText(url)
-            else:
-                jsonData = {"url": imageBaseURL + sys.argv[2]}
-                request = requests.post(APIURL, headers=headersURL, json=jsonData, timeout=10) 
-                reqHeader = request.headers
-                url = reqHeader['Operation-Location']
-                getText(url)
+    print ("Mode: " + mode)
+    print ("Args: "+ str(len(sys.argv)))
+    # how was the script called? If only one argument was given, is it mode or imagename?
+    if len(sys.argv) < 2 or (len(sys.argv) == 2 and (sys.argv[1] == 'local' or sys.argv[1] == 'URL')):
+        # if no image was specified, loop over every image in the project folder (localImagesPath)
+        for file in directory:
+            if file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg') or file.endswith('.bmp'):
+                print (file + ' :------------------------------------------------------------------')
+                postToCloud(mode, file)      
+    # if only image was specified, but not mode, post specified image with default mode       
+    elif (len(sys.argv) == 2 and sys.argv[1] != 'local' and sys.argv[1] != 'URL'):
+        postToCloud(mode, sys.argv[1])
+    # else corresponds to script call with 2 arguments, where mode is the first and image is the second
+    else:
+        postToCloud(mode, sys.argv[2])
 
-    except requests.exceptions.RequestException as e:
-        print ("RequestException: ")
-        print(e)
-    except Exception as e:
-        print ("Exception: ")
-        print(e)
-
-
-if(len(sys.argv) > 1):
-    if(sys.argv[1] == 'local'):
-        mode = 'local'
-
+# set mode
+if(len(sys.argv) > 1 and (sys.argv[1] == 'local' or sys.argv[1] == 'URL')):
+    mode = sys.argv[1]
 
 uploadIMG()
